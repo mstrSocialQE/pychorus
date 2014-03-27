f5f446de9e416c9cd3127457339fcca24e9240c0
Li, Peng <redalertlunr@163.com>
2014-03-27 20:24:59 +0800

    update chorusrun

--- ChorusCore/Samples/chorusrun.py
+++ ChorusCore/Samples/chorusrun.py
@@ -5,18 +5,17 @@ Created on Dec 28, 2013
 @author: Anduril
 '''
 import sys
-#import os
-#import optparse
-#from ProjectConfiguration import ProjectConfiguration
-#from ProjectExecution import RunTest
-#from LogServer import Level
+import os
+import optparse
+from ProjectConfiguration import ProjectConfiguration
+from ProjectExecution import RunTest
+from LogServer import Level
 #class MyProject(ProjectConfiguration):
-#    '''default initialization, can be reloaded'''
-##     def __init__(self,options):
-##         ProjectConfiguration.__init__(self, options)
-#    
-#    def __init__(self,options):
-#        self.options=options
+#    def __init__(self, argv):
+#        self.init_options()
+#        '''Add your own options here by self.parser.add_option(...)'''
+#        self.options, argv = self.parser.parse_args(argv)
+#        '''Add your functions called by the added options here'''
 #        self.set_output_folder()
 #        self.set_logserver(level=Level.debug)
 #        self.logserver.add_filehandler(level=Level.error,
@@ -24,6 +23,9 @@ import sys
 #        self.logserver.add_filehandler(level=Level.info,
 #                                       filepath=self.outputdir,filename="info.log")
 #        self.set_configfile()
+#    
+##    def __init__(self, argv):
+##        self.setup(argv)
 #        
 #    def start_test(self): 
 #        RunTest()
